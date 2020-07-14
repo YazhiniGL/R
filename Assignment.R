df2ac3927b15a36bdf2acbc25709bdde28765fa2
Yazh Ini <yazhinibalaji98@gmail.com>
2020-07-14 16:27:32 +0530

    Create Assignment.R

---
+++ Assignment.R
@@ -0,0 +1,32 @@
+## To make cache matrix and solve
+
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+##   To set,get and list
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
+
+
